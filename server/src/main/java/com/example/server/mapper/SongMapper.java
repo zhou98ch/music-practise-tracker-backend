4e997d5ed72d19b7656158cb03efde3b575bf2b3
zhou98ch <jingyuezhou@outlook.com>
2026-05-01 20:38:18 +0200

    fix: fix wrong mapping name in sql

--- server/src/main/java/com/example/server/mapper/SongMapper.java
+++ server/src/main/java/com/example/server/mapper/SongMapper.java
@@ -18,18 +18,18 @@ public interface SongMapper {
      *
      * @param song
      */
-    @Insert("insert into song (name, artist, description, created_user_id, is_archived, is_deleted, is_private, created_date, updated_date) " +
+    @Insert("insert into song (name, artist, description, createdUserId, isArchived, isDeleted, isPrivate, createdDate, updatedDate) " +
             "values (#{name}, #{artist}, #{description}, #{createdUserId}, #{isArchived}, #{isDeleted}, #{isPrivate}, #{createdDate}, #{updatedDate})")
     @AutoFill(value = OperationType.INSERT)
     int insert(Song song);
 
-    @Update("update song set is_deleted = 1, updated_date = #{updatedDate} where id = #{id}")
+    @Update("update song set isDeleted = 1, updatedDate = #{updatedDate} where id = #{id}")
     void deletebyID(Long id, LocalDate updatedDate);
-     @Update("update song set name = #{name}, artist = #{artist}, description = #{description}, is_archived = #{isArchived}, is_private = #{isPrivate}, updated_date = #{updatedDate} where id = #{id}")
+     @Update("update song set name = #{name}, artist = #{artist}, description = #{description}, isArchived = #{isArchived}, isPrivate = #{isPrivate}, updatedDate = #{updatedDate} where id = #{id}")
      @AutoFill(value = OperationType.UPDATE)
      void update(Song song);
 
-    @Select("select from song where id=#{id}")
+    @Select("select * from song where id=#{id}")
     Song selectById(Long id);
 
     /**
@@ -38,6 +38,6 @@ public interface SongMapper {
      * @param category_id
      * @return
      */
-    @Select("select * from song where category_id=#{category_id} order by created_date desc")
+    @Select("select * from song where categoryId=#{category_id} order by createdDate desc")
     Object listByCategory(Long category_id);
 }
